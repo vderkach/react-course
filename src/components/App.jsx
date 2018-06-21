374e216c4c90bd06ad1ae544dc547259a438458e
vderkach <veronika.derkach.88@gmail.com>
2018-06-22 02:14:33 +0300

    added some changes

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+  render() {
+    return (
+      <div className='container'>
+        Redux
+      </div>
+    )
+  }
+}
+
+export default App;
