b7a67da184d5caa121ae74ace6bc35f3b7dcbaa4
Veronika Derkach <veronika.derkach@bazaarvoice.com>
2018-06-22 12:42:35 +0300

    gallery committed

---
+++ src/components/ImageModal.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class ImageModal extends Component {
+  constructor() {
+    super()
+  }
+
+  render() {
+    return (console.log("dffddd"))
+  }
+}
+
+export default ImageModal;
