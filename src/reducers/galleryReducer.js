bf45cc36ca5f755e74adc2b4c4e489db92f86f37
Veronika Derkach <veronika.derkach@bazaarvoice.com>
2018-07-27 12:30:58 +0300

    updates

--- src/reducers/galleryReducer.js
+++ src/reducers/galleryReducer.js
@@ -5,45 +5,45 @@ export default function(state=reducers.defaultState, action) {
   switch(action.type) {
     case actions.FETCH_IMAGES: {
       return Object.assign({}, state, {
-              images: action.images,
-              totalImages: action.totalHits
-            });
+        images: action.images,
+        totalImages: action.totalHits
+      });
     }
     case actions.IMAGE_OPENED: {
       return Object.assign({}, state, {
-              selectedImage: action.selectedImage
-            });
+        selectedImage: action.selectedImage
+      });
     }
     case actions.UPDATE_TEXT: {
       return Object.assign({}, state, {
-              query: action.query
-            });
+        query: action.query
+      });
     }
     case actions.NEXT_PAGE: {
       return Object.assign({}, state, {
-              currentPage: state.currentPage + 1
-            });
+        currentPage: state.currentPage + 1
+      });
     }
     case actions.PREV_PAGE: {
       return Object.assign({}, state, {
-              currentPage: (state.currentPage > 1) ? state.currentPage - 1 : state.currentPage
-            });
+        currentPage: (state.currentPage > 1) ? state.currentPage - 1 : state.currentPage
+      });
     }
     case actions.RESET: {
       return Object.assign({}, state, {
-              currentPage: 1,
-              selectedImage: ''
-            });
+        currentPage: 1,
+        selectedImage: ''
+      });
     }
     case actions.START_FETCHING: {
       return Object.assign({}, state, {
-              isFetching: true
-            });
+        isFetching: true
+      });
     }
     case actions.FINISH_FETCHING: {
       return Object.assign({}, state, {
-              isFetching: false
-            });
+        isFetching: false
+      });
     }
     default:
       return state;
